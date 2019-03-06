2e0fbcafe2d1203aba9b845473ea813d66a81ba7
Erwin Rahayu <artmxarea@gmail.com>
2019-03-06 13:05:50 +0700

    add About Page

---
+++ .next/static/development/pages/about.js
@@ -0,0 +1,119 @@
+((window["webpackJsonp"] = window["webpackJsonp"] || []).push([["static\\development\\pages\\about.js"],{
+
+/***/ "./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fabout&absolutePagePath=D%3A%5C2.LAB%5CREACT%5CREACT%20JS%5Clandingpage%5Cpages%5Cabout.js!./":
+/*!************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fabout&absolutePagePath=D%3A%5C2.LAB%5CREACT%5CREACT%20JS%5Clandingpage%5Cpages%5Cabout.js ***!
+  \************************************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+
+    (window.__NEXT_P=window.__NEXT_P||[]).push(["/about", function() {
+      var page = __webpack_require__(/*! ./pages/about.js */ "./pages/about.js")
+      if(true) {
+        module.hot.accept(/*! ./pages/about.js */ "./pages/about.js", function() {
+          if(!next.router.components["/about"]) return
+          var updatedPage = __webpack_require__(/*! ./pages/about.js */ "./pages/about.js")
+          next.router.update("/about", updatedPage.default || updatedPage)
+        })
+      }
+      return { page: page.default || page }
+    }]);
+  
+
+/***/ }),
+
+/***/ "./node_modules/react/index.js":
+/*!*******************************************************************************************!*\
+  !*** delegated ./node_modules/react/index.js from dll-reference dll_55dc4e2ecf7824085104 ***!
+  \*******************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = (__webpack_require__(/*! dll-reference dll_55dc4e2ecf7824085104 */ "dll-reference dll_55dc4e2ecf7824085104"))("./node_modules/react/index.js");
+
+/***/ }),
+
+/***/ "./pages/about.js":
+/*!************************!*\
+  !*** ./pages/about.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "D:\\2.LAB\\REACT\\REACT JS\\landingpage\\pages\\about.js";
+
+
+var About = function About() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "About NextJS"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi quaerat quia minima fuga accusantium, enim dolorum! Tenetur explicabo nemo suscipit maiores. Pariatur temporibus eligendi modi assumenda omnis! Perferendis, doloremque aspernatur."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, "Omnis, eaque. Non pariatur facere itaque necessitatibus. Dicta ullam saepe voluptate quos accusamus placeat. Ex ullam, vitae harum nostrum enim odit assumenda debitis veniam illum magnam, itaque laboriosam corporis tempora."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }, "Temporibus assumenda cumque eius omnis officiis ut, voluptas eligendi maxime necessitatibus neque placeat quia, ducimus culpa reprehenderit labore perferendis molestias commodi. Beatae atque id quas, tempore culpa quidem nesciunt possimus.")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (About);
+
+/***/ }),
+
+/***/ 3:
+/*!****************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fabout&absolutePagePath=D%3A%5C2.LAB%5CREACT%5CREACT%20JS%5Clandingpage%5Cpages%5Cabout.js ***!
+  \****************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fabout&absolutePagePath=D%3A%5C2.LAB%5CREACT%5CREACT%20JS%5Clandingpage%5Cpages%5Cabout.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fabout&absolutePagePath=D%3A%5C2.LAB%5CREACT%5CREACT%20JS%5Clandingpage%5Cpages%5Cabout.js!./");
+
+
+/***/ }),
+
+/***/ "dll-reference dll_55dc4e2ecf7824085104":
+/*!*******************************************!*\
+  !*** external "dll_55dc4e2ecf7824085104" ***!
+  \*******************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = dll_55dc4e2ecf7824085104;
+
+/***/ })
+
+},[[3,"static/runtime/webpack.js"]]]));;
+//# sourceMappingURL=about.js.map
---
+++ .next/static/webpack/static/development/pages/about.js.6d4c98b8a67ac0c331f1.hot-update.js
@@ -0,0 +1,62 @@
+webpackHotUpdate("static\\development\\pages\\about.js",{
+
+/***/ "./pages/about.js":
+/*!************************!*\
+  !*** ./pages/about.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+var _jsxFileName = "D:\\2.LAB\\REACT\\REACT JS\\landingpage\\pages\\about.js";
+
+
+var About = function About() {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("section", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 2
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 3
+    },
+    __self: this
+  }, "About NextJS"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 4
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5
+    },
+    __self: this
+  }, "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Commodi quaerat quia minima fuga accusantium, enim dolorum! Tenetur explicabo nemo suscipit maiores. Pariatur temporibus eligendi modi assumenda omnis! Perferendis, doloremque aspernatur."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 11
+    },
+    __self: this
+  }, "Omnis, eaque. Non pariatur facere itaque necessitatibus. Dicta ullam saepe voluptate quos accusamus placeat. Ex ullam, vitae harum nostrum enim odit assumenda debitis veniam illum magnam, itaque laboriosam corporis tempora."), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("span", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 17
+    },
+    __self: this
+  }, "Temporibus assumenda cumque eius omnis officiis ut, voluptas eligendi maxime necessitatibus neque placeat quia, ducimus culpa reprehenderit labore perferendis molestias commodi. Beatae atque id quas, tempore culpa quidem nesciunt possimus.")));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (About);
+
+/***/ })
+
+})
+//# sourceMappingURL=about.js.6d4c98b8a67ac0c331f1.hot-update.js.map
